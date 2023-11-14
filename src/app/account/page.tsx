1a0d15e2df3edcac1802b162d567a5ad53fd3d46
mahadad-neutrix <mahadadhossain@neutrix.co>
2023-11-14 09:47:36 +0600

    Refactor account page component

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,16 +1,7 @@
 'use client';
-import { NextPage } from 'next';
 import { useSession } from 'next-auth/react';
-import { ReactNode } from 'react';
 
-interface PageProps {
-    children: ReactNode;
-}
-
-const Page: NextPage<PageProps> = ({
-    children,
-}) => {
-
+const Page = () => {
 
     const session = useSession();
     console.log("🚀 ~ file: page.tsx:16 ~ session:", session);
